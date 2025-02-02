2b2cf0004a2ff552f0b1b53b0f99cde7e542d5af
Andi Duro <nexuister@gmail.com>
2025-02-01 20:36:47 -0500

    Initial code

---
+++ dev.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+deno run --watch --allow-net --allow-read --allow-env --allow-run --no-prompt index.ts
--- index.ts
+++ index.ts
@@ -1,5 +1,10 @@
-// Set up web server
-// Set up hello world route
 // Set up Twilio
 // Log Twilio messages
 // Send hello world message
+
+const handler = async (request: Request): Promise<Response> => {
+  return new Response("Hello World");
+}
+
+Deno.serve({ port: 6601 }, handler);
+console.log("sms-bot running. Access it at: http://localhost:6601/");
